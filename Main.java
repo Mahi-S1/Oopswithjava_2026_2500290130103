959b8c7212f176e0235ac0a413eddc34621b003f
Mahi Sharma <mahisharma062024@gmail.com>
2026-09-08 09:37:02 +0530

    Add Hello World using separate class

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Hello {
+    void display() {
+        System.out.println("Hello, World!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.display();
+    }
+}
